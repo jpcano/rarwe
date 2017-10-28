99487e1eedb27472a5c3763316d4d32a3b84832c
Jesus Caño <jcanovel@gmail.com>
2017-10-29 00:32:24 +0200

    Finished chapter 11: Testing

---
+++ tests/acceptance/bands-test.js
@@ -0,0 +1,152 @@
+import {test} from 'qunit';
+import moduleForAcceptance from 'rarwe/tests/helpers/module-for-acceptance';
+import Pretender from 'pretender';
+
+moduleForAcceptance('Acceptance | bands', {
+  afterEach() {
+    server.shutdown();
+  }
+});
+
+var server;
+
+test('List bands', function (assert) {
+  server = new Pretender(function () {
+    this.get('/bands', function () {
+      var response = {
+        data: [
+          {
+            id: 1,
+            type: 'bands',
+            attributes: {
+              name: 'Radiohead'
+            }
+          },
+          {
+            id: 2,
+            type: 'bands',
+            attributes: {
+              name: 'Long Distance Calling'
+            }
+          },
+        ]
+      };
+      return [200, {'Content-Type': 'application/vnd.api+json'}, JSON.stringify(response)];
+    });
+  });
+
+  visit('/bands');
+
+  andThen(function () {
+    assert.equal(find('.band-link').length, 2, 'All band links are rendered');
+    assert.equal(find('.band-link:contains("Radiohead")').length, 1, 'First band link contains the band name');
+    assert.equal(find('.band-link:contains("Long Distance Calling")').length, 1, 'The other band link contains the band name');
+  });
+});
+
+test('Create a new band', function (assert) {
+  server = new Pretender(function () {
+    this.get('/bands', function () {
+      var response = {
+        data: [
+          {
+            id: 1,
+            type: 'bands',
+            attributes: {
+              name: 'Radiohead'
+            }
+          }
+        ]
+      };
+      return [200, {'Content-Type': 'application/vnd.api+json'},
+        JSON.stringify(response)];
+    });
+    this.post('/bands', function () {
+      var response = {
+        data: {
+          id: 2,
+          type: 'bands',
+          attributes: {
+            name: 'Long Distance Calling'
+          }
+        }
+      };
+      return [200, {'Content-Type': 'application/vnd.api+json'},
+        JSON.stringify(response)];
+    });
+    this.get('/bands/2/songs', function () {
+      var response = {
+        data: []
+      };
+      return [200, {'Content-Type': 'application/vnd.api+json'},
+        JSON.stringify(response)];
+    });
+  });
+  visit('/bands');
+  fillIn('.new-band', 'Long Distance Calling');
+  click('.new-band-button');
+  andThen(function () {
+    assertLength(assert, '.band-link', 2, 'All band links are rendered');
+    assertLength(assert, '.band-link:contains("Radiohead")', 1, 'First band link contains the band name');
+    assertLength(assert, '.band-link:contains("Long Distance Calling")', 1, 'The other band link contains the band name');
+  });
+});
+
+test('Create a new song in two steps', function (assert) {
+  server = new Pretender(function () {
+    this.get('/bands', function () {
+      var response = {
+        data: [
+          {
+            id: 1,
+            type: 'bands',
+            attributes: {
+              name: 'Radiohead'
+            }
+          }
+        ]
+      };
+      return [200, {'Content-Type': 'application/vnd.api+json'},
+        JSON.stringify(response)];
+    });
+    this.get('/bands/1', function () {
+      var response = {
+        data: {
+          id: 1,
+          type: 'bands',
+          attributes: {
+            name: 'Radiohead'
+          }
+        }
+      };
+      return [200, {'Content-Type': 'application/vnd.api+json'},
+        JSON.stringify(response)];
+    });
+    this.post('/songs', function () {
+      var response = {
+        data: {
+          id: 1,
+          type: 'songs',
+          attributes: {
+            name: 'Killer Cars'
+          }
+        }
+      };
+      return [200, {'Content-Type': 'application/vnd.api+json'},
+        JSON.stringify(response)];
+    });
+    this.get('/bands/1/songs', () => {
+      return [200, {'Content-Type': 'application/vnd.api+json'},
+        JSON.stringify({data: []})];
+    });
+  });
+
+  selectBand('Radiohead');
+  click('a:contains("create one")');
+  fillIn('.new-song', 'Killer Cars');
+  submit('.new-song-form');
+  andThen(function () {
+    assertElement(assert, '.songs .song:contains("Killer Cars")',
+      'Creates the song and displays it in the list');
+  });
+});
--- tests/integration/components/star-rating-test.js
+++ tests/integration/components/star-rating-test.js
@@ -1,24 +1,28 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import Ember from 'ember';
+import {moduleForComponent, test} from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('star-rating', 'Integration | Component | star rating', {
+moduleForComponent('star-rating', 'Integration | Component | star-rating', {
   integration: true
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+test('Renders the full and empty stars correctly', function (assert) {
+  assert.expect(6);
 
-  this.render(hbs`{{star-rating}}`);
+  var song = Ember.Object.create({rating: 4});
 
-  assert.equal(this.$().text().trim(), '');
+  this.set('song', song);
 
-  // Template block usage:
-  this.render(hbs`
-    {{#star-rating}}
-      template block text
-    {{/star-rating}}
-  `);
+  this.set('maxRating', 5);
+  this.render(hbs`{{star-rating item=song rating=song.rating maxRating=maxRating}}`);
+  assert.equal(this.$('.glyphicon-star').length, 4, 'The right amount of full stars is rendered');
+  assert.equal(this.$('.glyphicon-star-empty').length, 1, 'The right amount of empty stars is rendered');
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  this.set('maxRating', 10);
+  assert.equal(this.$('.glyphicon-star').length, 4, 'The right amount of full stars is rendered after changing maxRating');
+  assert.equal(this.$('.glyphicon-star-empty').length, 6, 'The right amount of empty stars is rendered after changing maxRating');
+
+  this.set('song.rating', 2);
+  assert.equal(this.$('.glyphicon-star').length, 2, 'The right amount of full stars is rendered after changing rating');
+  assert.equal(this.$('.glyphicon-star-empty').length, 8, 'The right amount of empty stars is rendered after changing rating');
 });

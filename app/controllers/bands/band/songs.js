90f36ededbd56aacfb642bbcf5f69d8d7647aa1b
Jesus Caño <jcanovel@gmail.com>
2017-10-28 17:11:52 +0200

    Finished chapter 8: Controllers

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,9 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-
+  songCreationStarted: false,
+  canCreateSong: Ember.computed('songCreationStarted',
+    'model.songs.length', function () {
+      return this.get('songCreationStarted') ||
+        this.get('model.songs.length');
+    }),
+  title: '',
+  isAddButtonDisabled: Ember.computed('title', function () {
+    return Ember.isEmpty(this.get('title'));
+  }),
   actions: {
-    updateRating: function(params) {
+    enableSongCreation: function () {
+      this.set('songCreationStarted', true);
+    },
+    updateRating: function (params) {
       var song = params.item,
         rating = params.rating;
       song.set('rating', rating);

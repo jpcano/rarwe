a70740330e98514f43e8d150298ad6b7431623cb
Jesus Caño <jcanovel@gmail.com>
2017-10-28 22:36:47 +0200

    Finished chapter 10: Talking to a backend

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -2,8 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function (params) {
-    console.log('Model hook called for `bands.band` called with', params.slug);
-    var bands = this.modelFor('bands');
-    return bands.findBy('slug', params.slug);
+    return this.store.findRecord('band', params.id);
   }
 });

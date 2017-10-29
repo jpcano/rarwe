f8312f76607c539e1c7f0a49ceaf29834389157d
Jesus Caño <jcanovel@gmail.com>
2017-10-29 17:02:14 +0100

    Finished chapter 12: Sorting and searching

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,20 +1,49 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  queryParams: {
+    sortBy: 'sort',
+    searchTerm: 's',
+  },
+
   songCreationStarted: false,
   canCreateSong: Ember.computed('songCreationStarted',
     'model.songs.length', function () {
       return this.get('songCreationStarted') ||
         this.get('model.songs.length');
     }),
+
   title: '',
   isAddButtonDisabled: Ember.computed('title', function () {
     return Ember.isEmpty(this.get('title'));
   }),
+
+  sortBy: 'ratingDesc',
+  sortProperties: Ember.computed('sortBy', function () {
+    var options = {
+      'ratingDesc': 'rating:desc,title:asc',
+      'ratingAsc': 'rating:asc,title:asc',
+      'titleDesc': 'title:desc',
+      'titleAsc': 'title:asc',
+    };
+    return options[this.get('sortBy')].split(',');
+  }),
+  sortedSongs: Ember.computed.sort('matchingSongs', 'sortProperties'),
+
+  searchTerm: '',
+  matchingSongs: Ember.computed('model.songs.@each.title', 'searchTerm', function () {
+    var searchTerm = this.get('searchTerm').toLowerCase();
+    return this.get('model.songs').filter(function (song) {
+      return song.get('title').toLowerCase().indexOf(searchTerm) !== -1;
+    });
+  }),
+
   actions: {
     enableSongCreation: function () {
       this.set('songCreationStarted', true);
     },
+
     updateRating: function (params) {
       var song = params.item,
         rating = params.rating;
@@ -24,6 +53,7 @@ export default Ember.Controller.extend({
       song.set('rating', rating);
       return song.save();
     }
+
   }
 
 });
